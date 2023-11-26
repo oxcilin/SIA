74025afe1f903f3b6f20db2b57018b5d7353603d
Oxa <96934856+oxcilin@users.noreply.github.com>
2023-11-27 06:39:45 +0700

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -2,9 +2,9 @@
 
 // Database configuration
 $servername = "localhost";  // Replace with your server name
-$username = "oxabizid_sia";  // Replace with your database username
-$password = "RPz^kz)u[Jee";  // Replace with your database password
-$dbname = "oxabizid_sia";       // Replace with your database name
+$username = "***";  // Replace with your database username
+$password = ">**";  // Replace with your database password
+$dbname = "***";       // Replace with your database name
 
 // Create a connection to the database
 $conn = mysqli_connect($servername, $username, $password, $dbname);
